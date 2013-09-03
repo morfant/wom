dc53a4bbc624beb94b2ecfa356f4f2745adf2be8
Gangil <git>
2013-09-04 01:24:42 +0900

    NDB Test

--- wom.py
+++ wom.py
@@ -1,3 +1,5 @@
+# -*- coding:utf-8 -*-
+import sys
 import cgi
 import urllib
 import webapp2
@@ -9,8 +11,14 @@ from google.appengine.ext import ndb
 MAIN_PAGE_HTML = """\
 <html>
 	<body>
+
+        keyword:
+        <div><input type="text" size="100" name="userInput1"></div>
+        <div><input type='submit' value='input'></div>
 		<form method="post">
-		<div><input type="text" size="100" name="userInput"></div>
+        content:
+        <div><input type="text" size="100" name="userInput2"></div>
+        <div><input type='submit' value='input'></div>
 		</form>
 	</body>
 </html>
@@ -21,44 +29,63 @@ def makeKey(wordToTrans = DEFAULT):
 	return ndb.Key("Words", wordToTrans)
 
 class WOM(ndb.Model):
-	content = ndb.StringProperty(indexed=False)
-	date = ndb.DateTimeProperty(auto_now_add=True)
-		
+    keyword = ndb.StringProperty(indexed=True)
+    content = ndb.StringProperty(indexed=True)
+    date = ndb.DateTimeProperty(auto_now_add=True)
 
 class MainPage(webapp2.RequestHandler):
     
+    print (sys.getdefaultencoding())
+
     def get(self):
-		self.response.write(MAIN_PAGE_HTML)
+        print (u'안녕씹'.encode('utf-8'))
+        self.response.write(MAIN_PAGE_HTML)
 
-		wordToTrans = self.request.get('userInput', DEFAULT)
-		transQuery = WOM.query(
-    		ancestor=makeKey(wordToTrans)).order(-WOM.date)
-		transedWords = transQuery.fetch(10)
+        wordToTrans = self.request.get('userInput', DEFAULT)
 
-		for transedWord in transedWords:
-			if transedWord.content:
-				self.response.write('<b>%s</b>' % transedWord.content)
+        print("1: %s" % wordToTrans)
+
+        transQuery = WOM.query(
+            ancestor=makeKey(wordToTrans)).order(-WOM.date)
+        transedWords = transQuery.fetch(10)
+
+        for transedWord in transedWords:
+            if transedWord.content:
+                self.response.write('<b>%s</b>' % transedWord.content)
 
     def post(self):
-        self.response.write("kkkkk")
+        userInputVal = self.request.get(u'userInput2', DEFAULT)
+        print("userInputVal: %s %s %s" % (userInputVal, type(userInputVal), unicode(userInputVal,'utf-8')))
+        wom = WOM(parent=makeKey(unicode(userInputVal,'utf-8')))
+        wom.content = self.request.get('userInput2')
+        wom.put()
+        self.response.write('<b>%s</b> puted.' % wom.content)
+
+        self.response.write('<b>%s</b> list.' % wom)
+
+
 
 class SecondPage(webapp2.RequestHandler):
+    
+    def post(self):
 
-	def get(self):
-		user = users.get_current_user()
+        wordToTrans = self.request.get('userInput', DEFAULT)
+        print("2: %s" % wordToTrans)
+        print (wordToTrans.encode('utf-8'))
+        wom = WOM(parent=makeKey(wordToTrans))
+        wom.keyword = self.request.get('userInput1')
+        wom.content = self.request.get('userInput2')
+        wom.put()
 
-		self.response.headers['Content-Type'] = 'text/plain'
-		self.response.write('Hello, Again!\n')
+        transQuery = WOM.query(
+            ancestor=makeKey(wordToTrans)).order(-WOM.date)
 
-		if user:
-			self.redirect(users.create_logout_url(self.request.uri))
-		else:
-			self.response.write("login please")
+        transedWords = transQuery.fetch(10)
+        print transedWords        
+        for transedWord in transedWords:
+            if transedWord.content:
+                self.response.write('<b>%s</b>' % transedWord.content)
 
-	def post(self):
-		self.response.write('<html><body>You wrote: <pre>')
-		self.response.write(cgi.escape(self.request.get('userInput')))
-		self.response.write('</pre></body></html>')
 
 
 
