de14a1309957185d7e9ead7acd99c28e1e0ce540
Gangil <git>
2013-09-04 17:04:02 +0900

    Input and return

--- wom.py
+++ wom.py
@@ -9,24 +9,44 @@ from google.appengine.ext import ndb
 
 
 MAIN_PAGE_HTML = """\
+<!DOCTYPE html>
 <html>
-	<body>
+    <head>
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8" >
 
+    </head>
+
+    <body>
+        <form method="post" action="/test">
         keyword:
-        <div><input type="text" size="100" name="userInput1"></div>
-        <div><input type='submit' value='input'></div>
-		<form method="post">
+        <div id = "demo"><input type="text" size="100" name="userInput1"></div>
         content:
-        <div><input type="text" size="100" name="userInput2"></div>
-        <div><input type='submit' value='input'></div>
-		</form>
-	</body>
+        <div id = "demo3"><input type="text" size="100" name="userInput2"></div>
+        <div id = "demo2"><input type='submit' value='input'></div>
+        </form>
+    </body>
+</html>
+"""
+
+SECOND_PAGE_HTML = """\
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8" >
+
+    </head>
+
+    <body>
+        <form method="GET" action="/">
+            <div id = "demo2"><input type='submit' value='RETURN'></div>
+        </form>
+    </body>
 </html>
 """
 DEFAULT = "hello"
 
 def makeKey(wordToTrans = DEFAULT):
-	return ndb.Key("Words", wordToTrans)
+    return ndb.Key("Words", wordToTrans)
 
 class WOM(ndb.Model):
     keyword = ndb.StringProperty(indexed=True)
@@ -38,7 +58,6 @@ class MainPage(webapp2.RequestHandler):
     print (sys.getdefaultencoding())
 
     def get(self):
-        print (u'안녕씹'.encode('utf-8'))
         self.response.write(MAIN_PAGE_HTML)
 
         wordToTrans = self.request.get('userInput', DEFAULT)
@@ -69,23 +88,27 @@ class SecondPage(webapp2.RequestHandler):
     
     def post(self):
 
-        wordToTrans = self.request.get('userInput', DEFAULT)
-        print("2: %s" % wordToTrans)
-        print (wordToTrans.encode('utf-8'))
+        #print("keyword : %s" % self.request.get('userInput1')) #한글을 입력하면 문제를 일으킨다
+        #print("content : %s" % self.request.get('userInput2'))
+        wordToTrans = self.request.get('userInput1', DEFAULT)
+        #print("2: %s" % wordToTrans)
+        #print (wordToTrans.encode('utf-8'))
         wom = WOM(parent=makeKey(wordToTrans))
         wom.keyword = self.request.get('userInput1')
         wom.content = self.request.get('userInput2')
         wom.put()
 
-        transQuery = WOM.query(
-            ancestor=makeKey(wordToTrans)).order(-WOM.date)
-
+        #transQuery = WOM.query(ancestor=makeKey(wordToTrans)).order(-WOM.date)
+        transQuery = WOM.query().order(-WOM.date)
         transedWords = transQuery.fetch(10)
-        print transedWords        
+
+        #print transedWords        
         for transedWord in transedWords:
             if transedWord.content:
-                self.response.write('<b>%s</b>' % transedWord.content)
+                self.response.write('keyword: %s / content: %s <br>' % (transedWord.keyword, transedWord.content))
 
+        self.response.write(SECOND_PAGE_HTML)
+        self.response.write('<img src=/img/test.png />')
 
 
 
