0cf5a80bb01aa3c1415f84b47246d7ad3fea40b9
Gangil <git>
2014-01-31 01:52:54 +0900

    0131_widely modified

--- wom.py
+++ wom.py
@@ -24,6 +24,9 @@ DATA_FILE_4 = "data_4.txt"
 keys = ""
 keys_ENG = ""
 
+class StaticKeys :
+    keys = ""
+    keys_ENG = ""
 
 #################################### HTML ####################################
 MAIN_PAGE_HTML = """\
@@ -44,6 +47,11 @@ MAIN_PAGE_HTML = """\
                 font-size: 10px;
                 line-height: 0.1;
                 }
+            p {
+                text-align: left;
+                font-size: 12px;
+
+            }
         </style>
 
     </head>
@@ -55,9 +63,16 @@ MAIN_PAGE_HTML = """\
                     <input type="text" size="100" name="searchingWord">
                 </div>
                 <div style="position: relative; top: 50px" align="center">
-                    <input type="submit" value="번역">
+                    <input type="submit" value="번역 / Translate">
                 </div>
             </form>
+            <div style="position: relative; left: 400px; top: 200px">
+                <p>
+                한국어/영어 입력 가능합니다.
+                <br>
+                한국어 입력 시에는 한국어 결과가, 영어 입력시에는 영어 결과가 출력됩니다.
+                </p>
+            </div>
         </div>
     </body>
 
@@ -119,12 +134,13 @@ class WOM(ndb.Model) :
     keyword = ndb.StringProperty(indexed=True)
     content = ndb.StringProperty(indexed=True)
     date = ndb.DateTimeProperty(auto_now_add=True)
+    idx = ndb.IntegerProperty(indexed=True)
 
 class WOM_ENG(ndb.Model) :
     keyword = ndb.StringProperty(indexed=True)
     content = ndb.StringProperty(indexed=True)
     date = ndb.DateTimeProperty(auto_now_add=True)
-
+    idx = ndb.IntegerProperty(indexed=True)
 
 #################################### PAGES ####################################
 class MainPage(webapp2.RequestHandler) :
@@ -167,12 +183,20 @@ class SetupDB(webapp2.RequestHandler) :
     def get(self) :
         global keys
         global keys_ENG
-        keys = makeKeyList()
-        keys_ENG = makeKeyList_ENG()
-        for key in keys :
-            print ("key %s" % key)
-        for key in keys_ENG :
-            print ("key_ENG %s" % key)
+
+        # keys = makeKeyList()
+        # keys_ENG = makeKeyList_ENG()
+        # for key in keys :
+        #     print ("key %s" % key)
+        # for key in keys_ENG :
+        #     print ("key_ENG %s" % key)
+
+
+        StaticKeys.keys = makeKeyList()
+        StaticKeys.keys_ENG = makeKeyList_ENG()
+
+        print StaticKeys.keys
+        print StaticKeys.keys_ENG
 
 
         self.response.write("DB is set.")
@@ -182,21 +206,36 @@ class FindDB(webapp2.RequestHandler) :
     def get(self) :
         global keys
         global keys_ENG
+        lenOfKORWords = len(StaticKeys.keys)
+        lenOfENGWords = len(StaticKeys.keys_ENG)
         toFindstrLists = []
         #os.system('say wait')
         #print sys.platform
-        imgPrinted = False
+        # imgPrinted = False
         toFindstr = ' '
-        numOfmatch = 0
+        # numOfmatch = 0
         #print keys
         toFind = self.request.get('searchingWord') #unicode
         if toFind == "" :
             # print ("nothing is in.")
             randImgDisplay(self, 34)
         else :
-            if len(keys) == 0 :
-                keys = makeKeyList()
-                keys_ENG = makeKeyList_ENG()
+            # if len(keys) == 0 :
+            #     keys = makeKeyList()
+            #     keys_ENG = makeKeyList_ENG()
+
+            if lenOfKORWords == 0 :
+                print ("len of KOR words is 0")
+                StaticKeys.keys = makeKeyList()
+                lenOfKORWords = len(StaticKeys.keys)
+                print StaticKeys.keys
+            if lenOfENGWords == 0 :
+                print ("len of ENG words is 0")
+                StaticKeys.keys_ENG = makeKeyList_ENG()
+                lenOfENGWords = len(StaticKeys.keys_ENG)
+                print StaticKeys.keys_ENG
+
+
 
             toFindstr = toFind.encode('utf-8')
             toFindstrList = toFindstr.split(' ')
@@ -205,68 +244,125 @@ class FindDB(webapp2.RequestHandler) :
             # print ("toFindstrLists: %s" % toFindstrLists)
             resultSentence = " "
 
-            for word in toFindstrLists: #split by space
-                # print ("\nFinding word: %s" % word)
-
-                numOfmatchInWord = 0;
-
-                if isKorean(word) :
-                    for key in keys :
-                        # print ("key: %s" % key)
-                        # print("isKorean: %s" % isKorean(word))
-                        if word.find(key) is not -1: #something matched in KOREAN
-                            #print("Matched in KOREAN!")
-                            randomResultStr = randDBOut(WOM, key)
-                            #print ("content_K: %s" % randomResultStr)
-                            resultSentence = resultSentence + word.replace(key, randomResultStr)
-                            #print ("resultSentence", resultSentence)
-
-                            numOfmatchInWord = numOfmatchInWord + 1
-                            numOfmatch = numOfmatch + 1
-                            break
-
+            if isKoreanToList(toFindstrList) == 0 : #All kor words
+                if isExactlyMatchToList(toFindstrList, StaticKeys.keys) :
+                    for word in toFindstrList: #split by space
+                        result = randDBOutInMatchingWord(WOM, word)
+                        self.response.write('<div align = "center", padding-top: 50px>' \
+                        + result + '</div>')
                 else :
-                    for key in keys_ENG :
-                        if word.find(key + " ") is not -1: #something matched in ENGLISH
-                            #print("Matched in ENGLISH!")
-                            randomResultStr = randDBOut(WOM_ENG, key)
-                            #print ("content_E: %s" % randomResultStr)
-                            resultSentence = resultSentence + " " + randomResultStr
-                            #print ("resultSentence", resultSentence)
+                    resultSentence = randDBOutInRandomWord(WOM, lenOfKORWords)
+                    self.response.write('<div align = "center", padding-top: 50px>' \
+                    + resultSentence + '</div>')
 
-                            numOfmatchInWord = numOfmatchInWord + 1
-                            numOfmatch = numOfmatch + 1
-                            break
-
-                if numOfmatchInWord == 0 :
-                    #print("Nothing matched in one DB loop with %s" % word)
-                    resultSentence = resultSentence + " " + word
-
-            if numOfmatch :
-                self.response.write('<div align = "center", padding-top: 50px>' + resultSentence + '</div>')
-            else :
-                #print ("Nothing matched at all!\n")
-                if coin() == 1 and len(keys) != 0 and len(keys_ENG) != 0: #random DB out
-                    #print ("situation 1\n")
-                    if coin() == 1 : #for korean
-                        #print ("situation 1 - 1\n")
-                        randomKeyIdx = random.randint(1, len(keys) - 1)
-                        resultSentence = randDBOut(WOM, keys[randomKeyIdx])
+            elif isKoreanToList(toFindstrList) == 1 : #All eng words
+                if isExactlyMatchToList(toFindstrList, StaticKeys.keys_ENG) :
+                    for word in toFindstrList: #split by space
+                        result = randDBOutInMatchingWord(WOM_ENG, word)
                         self.response.write('<div align = "center", padding-top: 50px>' \
-                            + resultSentence + '</div>')
+                        + result + '</div>')
+                else :
+                    resultSentence = randDBOutInRandomWord(WOM_ENG, lenOfENGWords)
+                    self.response.write('<div align = "center", padding-top: 50px>' \
+                    + resultSentence + '</div>')
 
-                    else : #for Eng
-                        #print ("situation 1 - 2\n")
-                        randomKeyIdx = random.randint(1, len(keys_ENG) - 1)
-                        resultSentence = randDBOut(WOM_ENG, keys_ENG[randomKeyIdx])
-                        self.response.write('<div align = "center", padding-top: 50px>' \
-                            + resultSentence + '</div>')
+            elif isKoreanToList(toFindstrList) == 2 : #kor + eng
+                randImgDisplay(self, 34)
 
-                else : # random Img out
-                    if imgPrinted is False : #준비된 이미지를 랜덤하게 선택해서 보여준다.
-                        #print ("situation 2\n")
-                        randImgDisplay(self, 34) #34 = max image number
-                        imgPrinted = True
+            # else :
+            #     if isExactlyMatchToList(toFindstrList, StaticKeys.keys) :
+            #         for word in toFindstrList: #split by space
+            #             # print ("\nFinding word: %s" % word)
+
+            #             # numOfmatchInWord = 0;
+
+            #             if isKorean(word) :
+            #                 result = randDBOutInMatchingWord(WOM, word)
+            #                 self.response.write('<div align = "center", padding-top: 50px>' \
+            #                 + result + '</div>')
+
+            #             else: #for eng
+            #                 resultSentence = randDBOutInMatchingWord(WOM_ENG, word)
+            #                 self.response.write('<div align = "center", padding-top: 50px>' \
+            #                 + resultSentence + '</div>')
+            #     else :
+            #         if isKorean()
+
+            #                     randomKeyIdx = random.randint(1, len(StaticKeys.keys) - 1)
+            #                     resultSentence = randDBOutInMatchingWord(WOM, StaticKeys.keys[randomKeyIdx])
+            #                     self.response.write('<div align = "center", padding-top: 50px>' \
+            #                     + resultSentence + '</div>')                        
+                            # # for key in keys :
+                            # for key in StaticKeys.keys :
+                            #     # print ("key: %s" % key)
+                            #     # print("isKorean: %s" % isKorean(word))
+                            #     if word.find(key) is not -1: #something matched in KOREAN
+                            #         #print("Matched in KOREAN!")
+                            #         randomResultStr = randDBOutInMatchingWord(WOM, key)
+                            #         #print ("content_K: %s" % randomResultStr)
+                            #         resultSentence = resultSentence + word.replace(key, randomResultStr)
+                            #         #print ("resultSentence", resultSentence)
+
+                            #         numOfmatchInWord = numOfmatchInWord + 1
+                            #         numOfmatch = numOfmatch + 1
+                            #         break
+
+                        # else : #english word
+
+
+                            # else:
+                            #     randomKeyIdx = random.randint(1, len(StaticKeys.keys_ENG) - 1)
+                            #     resultSentence = randDBOutInMatchingWord(WOM_ENG, StaticKeys.keys_ENG[randomKeyIdx])
+                            #     self.response.write('<div align = "center", padding-top: 50px>' \
+                            #     + resultSentence + '</div>')                        
+
+                    # # for key in keys_ENG :
+                    # for key in StaticKeys.keys_ENG :
+                    #     if word.find(key + " ") is not -1: #something matched in ENGLISH
+                    #         #print("Matched in ENGLISH!")
+                    #         randomResultStr = randDBOutInMatchingWord(WOM_ENG, key)
+                    #         #print ("content_E: %s" % randomResultStr)
+                    #         resultSentence = resultSentence + " " + randomResultStr
+                    #         #print ("resultSentence", resultSentence)
+
+                    #         numOfmatchInWord = numOfmatchInWord + 1
+                    #         numOfmatch = numOfmatch + 1
+                    #         break
+
+                # if numOfmatchInWord == 0 :
+                #     #print("Nothing matched in one DB loop with %s" % word)
+                #     resultSentence = resultSentence + " " + word
+
+            # if numOfmatch :
+            #     self.response.write('<div align = "center", padding-top: 50px>' + resultSentence + '</div>')
+            # else :
+            #     #print ("Nothing matched at all!\n")
+            #     # if coin() == 1 and len(keys) != 0 and len(keys_ENG) != 0: #random DB out
+            #     if coin() == 1 and len(StaticKeys.keys) != 0 and len(StaticKeys.keys_ENG) != 0: #random DB out
+            #         #print ("situation 1\n")
+            #         if isKorean(word) : #for KOR
+            #             #print ("situation 1 - 1\n")
+            #             # randomKeyIdx = random.randint(1, len(keys) - 1)
+            #             # resultSentence = randDBOutInMatchingWord(WOM, keys[randomKeyIdx])
+            #             randomKeyIdx = random.randint(1, len(StaticKeys.keys) - 1)
+            #             resultSentence = randDBOutInMatchingWord(WOM, StaticKeys.keys[randomKeyIdx])
+            #             self.response.write('<div align = "center", padding-top: 50px>' \
+            #                 + resultSentence + '</div>')
+
+            #         else : #for Eng
+            #             #print ("situation 1 - 2\n")
+            #             # randomKeyIdx = random.randint(1, len(keys_ENG) - 1)
+            #             # resultSentence = randDBOutInMatchingWord(WOM_ENG, keys_ENG[randomKeyIdx])
+            #             randomKeyIdx = random.randint(1, len(StaticKeys.keys_ENG) - 1)
+            #             resultSentence = randDBOutInMatchingWord(WOM_ENG, StaticKeys.keys_ENG[randomKeyIdx])
+            #             self.response.write('<div align = "center", padding-top: 50px>' \
+            #                 + resultSentence + '</div>')
+
+            #     else : # random Img out
+            #         if imgPrinted is False : #준비된 이미지를 랜덤하게 선택해서 보여준다.
+            #             #print ("situation 2\n")
+            #             randImgDisplay(self, 34) #34 = max image number
+            #             imgPrinted = True
 
         self.response.out.write(FIND_PAGE_HTML)
 
@@ -280,7 +376,60 @@ def randImgDisplay(target, maxImgNum) :
         <img src=/img/' + str(ranImgNum) + '.png height = "400" width = "400"/></div>')
 
 
-def randDBOut(dataBase, matchingWord) :
+def isExactlyMatch(word, DBList) :
+    print len(DBList)
+    lenOfDB = len(DBList)
+    for j in range(0, lenOfDB) :
+        # print wordList[i]
+        print j
+        print DBList[j]
+        if DBList[j] == word :
+            print "Exactly same!!"
+            return True
+        else:
+            if j == (lenOfDB - 1) :
+                print "Not same!!"
+                return False
+
+def isExactlyMatchToList(wordList, DBList) :
+    same = 0
+    # print len(DBList)
+    # print len(wordList)
+    lenOfWordList = len(wordList)
+    lenOfDB = len(DBList)
+    for i in range(0, lenOfWordList) :
+        for j in range(0, lenOfDB) :
+            # print wordList[i]
+            # print j
+            # print DBList[j]
+            if DBList[j] == wordList[i] :
+                same = same + 1
+                break
+        print ("same : %s " % same)
+
+    if same == lenOfWordList :
+        print "Exactly same!!"
+        return True
+    else :
+        print "Not same!!"
+        return False
+
+
+def randDBOutInRandomWord(dataBase, DBLength) :
+    # print ("DBLength : %s" % DBLength)
+    randomIdx = random.randint(0, DBLength - 1)
+    # print randomIdx
+    womquery = dataBase.query(dataBase.idx == randomIdx)
+    queryReturn = womquery.fetch(1)
+    # print queryReturn
+    resultContentList = queryReturn[0].content.split(' _ ')
+    # print resultContentList
+    randomResult = resultContentList[random.randint(0, len(resultContentList) - 1)]
+    randomResultStr = randomResult.encode('utf-8')
+    return randomResultStr
+
+
+def randDBOutInMatchingWord(dataBase, matchingWord) :
     womquery = dataBase.query(dataBase.keyword == matchingWord)
     queryReturn = womquery.fetch(1)
     resultContentList = queryReturn[0].content.split(' _ ')
@@ -303,6 +452,8 @@ def clearExistingDB() :
     ndb.get_context().clear_cache()
 
 def readFileToNdb(numOfDB) :
+    korIdx = 0
+    engIdx = 0
     if numOfDB == 1:
         f = open(DATA_FILE_1, 'r')
     elif numOfDB == 2:
@@ -326,16 +477,22 @@ def readFileToNdb(numOfDB) :
             wom = WOM(parent = makeKey(key))
             wom.keyword = key
             wom.content = escapeNvalue
+            wom.idx = korIdx
             wom.put()
+            korIdx = korIdx + 1
 
         else :
             wom = WOM_ENG(parent = makeKey(key))
             wom.keyword = key
             wom.content = escapeNvalue
+            wom.idx = engIdx
             wom.put()
+            engIdx = engIdx + 1
     f.close()
 
 def readAllDataFileToNdb() :
+    korIdx = 0
+    engIdx = 0
     key_list = []
     f = open(DATA_FILE, 'r')
     lines = f.readlines()
@@ -351,19 +508,52 @@ def readAllDataFileToNdb() :
             wom = WOM(parent = makeKey(key))
             wom.keyword = key
             wom.content = escapeNvalue
+            wom.idx = korIdx
             wom.put()
+            korIdx = korIdx + 1
+
 
         else :
             wom = WOM_ENG(parent = makeKey(key))
             wom.keyword = key
             wom.content = escapeNvalue
+            wom.idx = engIdx
             wom.put()
+            engIdx = engIdx + 1
     f.close()
 
 
 #정규표현식을 이용하여 한글이 포함되어 있는지를 판단한다.
 def isKorean(word) :
     return bool(re.search(r'(([\x7f-\xfe])+)', word))
+
+def isKoreanToList(wordList) :
+    numOfWord = len(wordList)
+    # print numOfWord
+    kor = 0
+    eng = 0
+    if numOfWord != 0 :
+        for word in wordList :
+            if isKorean(word) :
+                kor = kor + 1
+                # print ("kor : %s " % kor)
+            else :
+                eng = eng + 1
+                # print ("eng : %s " % eng)
+
+        if eng == 0 :
+            print "The wordlist is korean"
+            return 0 #All kor
+
+        elif kor == 0 :
+            print "The wordlist is ENGLISH"
+            return 1 #All eng
+
+        else :
+            print "The wordlist is korean + ENGLISH"
+            return 2 #kor + eng
+
+
 
 #긴 단어부터, 뒷 철자부터 정열된 keyList 를 만든다.
 def makeKeyList() :
